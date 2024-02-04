4e811fc7888f0f49191efd3ed66fb4d32660c31a
Miftahularif Hidayah <arieftheluffy@gmail.com>
2024-02-04 11:20:33 +0800

    layout-pelatihan

--- lib/module/pelatihan/view/pelatihan_view.dart
+++ lib/module/pelatihan/view/pelatihan_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:layout/shared/util/validator/validator.dart';
+import 'package:layout/shared/widget/form/category_picker/category_picker.dart';
 import '../controller/pelatihan_controller.dart';
 import '../state/pelatihan_state.dart';
 import 'package:get_it/get_it.dart';
@@ -58,22 +60,113 @@ class _PelatihanViewState extends State<PelatihanView> {
       appBar: AppBar(
         title: const Text('Pelatihan'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text(
-            'Counter: ${state.counter}',
-            style: const TextStyle(fontSize: 24),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 6.0,
+                  horizontal: 12.0,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(12.0),
+                  ),
+                  border: Border.all(
+                    width: 1.0,
+                    color: Colors.grey[400]!,
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(Icons.search),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        initialValue: null,
+                        decoration: const InputDecoration.collapsed(
+                          filled: true,
+                          fillColor: Colors.transparent,
+                          hintText: "Search",
+                          hoverColor: Colors.transparent,
+                        ),
+                        onFieldSubmitted: (value) {},
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.sort,
+                          size: 20.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              QCategoryPicker(
+                items: [
+                  {
+                    "label": "PKA",
+                    "value": "PKA",
+                  },
+                  {
+                    "label": "PKP",
+                    "value": "PKP",
+                  },
+                  {
+                    "label": "LATSAR",
+                    "value": "LATSAR",
+                  },
+                  {
+                    "label": "PPPK",
+                    "value": "PPPK",
+                  },
+                  {
+                    "label": "Pelatihan Teknis",
+                    "value": "Teknis",
+                  },
+                  {
+                    "label": "Sharing Season",
+                    "value": "sSeason",
+                  }
+                ],
+                validator: Validator.required,
+                onChanged: (index, label, value, item) {},
+              ),
+              Column(
+                children: [
+                  ListView.builder(
+                    itemCount: 30,
+                    shrinkWrap: true,
+                    physics: const ScrollPhysics(),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: Colors.grey[200],
+                            backgroundImage: const NetworkImage(
+                              "https://i.ibb.co/QrTHd59/woman.jpg",
+                            ),
+                          ),
+                          title: const Text("Jessica Doe"),
+                          subtitle: const Text("Programmer"),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ],
           ),
-          IconButton(
-            onPressed: () => controller.increment(),
-            icon: const Icon(
-              Icons.add,
-              size: 24.0,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
